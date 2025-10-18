4434335180f124e61470aaefc10a7be64535b2b7
Al-AminJoy <alaminislam3555@gmail.com>
2025-10-18 19:20:45 +0600

    App Bar Added

--- app/src/main/java/com/alamin/newsapp/ui/screen/MainActivity.kt
+++ app/src/main/java/com/alamin/newsapp/ui/screen/MainActivity.kt
@@ -7,24 +7,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.alamin.newsapp.ui.navigation.Destination
 import com.alamin.newsapp.ui.navigation.NavGraph
-import com.alamin.newsapp.ui.screen.home.HomeScreen
 import com.alamin.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NewsAppTheme {
-                Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
+                Scaffold(topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(text = "News Explorer")
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            titleContentColor = MaterialTheme.colorScheme.onPrimary
+                        )
+                    )
+                }, modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
                         App()
                     }
--- app/src/main/java/com/alamin/newsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/alamin/newsapp/ui/theme/Theme.kt
@@ -12,15 +12,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary,
+    onPrimary = White,
+    secondary = Secondary,
+    tertiary = Tertiary,
+    background = DarkBackground,
+    onBackground = OnDarkBackground,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary,
+    onPrimary = White,
+    secondary = Secondary,
+    tertiary = Tertiary,
+    background = LightBackground,
+    onBackground = OnLightBackground,
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +44,7 @@ private val LightColorScheme = lightColorScheme(
 fun NewsAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
