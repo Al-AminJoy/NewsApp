dff31b6acfccf6a79124c65bed6c7f6f27fd9e60
Al-AminJoy <alaminislam3555@gmail.com>
2025-11-01 14:42:16 +0600

    Component Changed For DI

--- app/src/main/java/com/alamin/newsapp/data/di/RepositoryModule.kt
+++ app/src/main/java/com/alamin/newsapp/data/di/RepositoryModule.kt
@@ -5,15 +5,14 @@ import com.alamin.newsapp.domain.repository.NewsRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 abstract class RepositoryModule {
-
     @Binds
     abstract fun bindNewsRepository(
         newsRepositoryImpl: NewsRepositoryImpl
     ): NewsRepository
-
 }
